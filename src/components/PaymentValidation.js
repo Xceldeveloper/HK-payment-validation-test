9e3ebe7c7886e3e324d238f4e9ecc9c0cbac3e79
Admin <admin.push@hackerrank.workspace>
2025-06-20 00:14:40 +0000

    Auto save changes before Switch to Local IDE
    Commit by HackerRank

--- src/components/PaymentValidation.js
+++ src/components/PaymentValidation.js
@@ -2,6 +2,18 @@ import React from "react";
 import "./PaymentValidation.css";
 
 const PaymentValidation = () => {
+  const [form,setForm] = React.useState({
+    cardNumber:"",
+    cardName:"",
+    cardExpiryMonth:"",
+    cardExpiryYear:"",
+    cardCVV:""
+  });
+
+
+
+
+
   return (
     <div className="mt-30 layout-column justify-content-center align-items-center">
       <div className="card outlined" style={{ width: "650px" }}>
@@ -30,32 +42,71 @@ const PaymentValidation = () => {
                 <input
                   placeholder="Card Number"
                   data-testid="cardNumberInput"
+                  value={form.cardNumber}
+                  onChange={(e)=>{
+                    setForm({
+                      ...form,
+                      cardNumber:e.target.value
+                    })
+                  }}
                 />
                 <p className="invalid-text" data-testid="numberInputError">
                   Invalid Card Number
                 </p>
               </div>
               <div className="layout-column mb-15">
-                <input placeholder="Name On Card" data-testid="nameInput" />
+                <input placeholder="Name On Card" data-testid="nameInput"
+                  value={form.cardName}
+                  onChange={(e)=>{
+                    setForm({
+                      ...form,
+                      cardName:e.target.value
+                    })
+                  }}
+                />
                 <p className="invalid-text" data-testid="nameInputError">
                   Invalid Card Name
                 </p>
               </div>
               <div className="flex justify-content-around align-items-center">
                 <div className="layout-column mb-30">
-                  <input placeholder="Expiry Month" data-testid="monthInput" />
+                  <input placeholder="Expiry Month" data-testid="monthInput"
+                   value={form.cardExpiryMonth}
+                  onChange={(e)=>{
+                    setForm({
+                      ...form,
+                      cardExpiryMonth:e.target.value
+                    })
+                  }}
+                  />
                   <p className="invalid-text" data-testid="monthInputError">
                     Invalid Month
                   </p>
                 </div>
                 <div className="layout-column mb-30">
-                  <input placeholder="Expiry Year" data-testid="yearInput" />
+                  <input placeholder="Expiry Year" data-testid="yearInput" 
+                   value={form.cardExpiryYear}
+                  onChange={(e)=>{
+                    setForm({
+                      ...form,
+                      cardExpiryYear:e.target.value
+                    })
+                  }}
+                  />
                   <p className="invalid-text" data-testid="yearInputError">
                     Invalid Year
                   </p>
                 </div>
                 <div className="layout-column mb-30">
-                  <input placeholder="CVV" data-testid="cvvInput" />
+                  <input placeholder="CVV" data-testid="cvvInput"
+                   value={form.cardCVV}
+                  onChange={(e)=>{
+                    setForm({
+                      ...form,
+                      cardCVV:e.target.value
+                    })
+                  }}
+                  />
                   <p className="invalid-text" data-testid="cvvInputError">
                     Invalid CVV
                   </p>
